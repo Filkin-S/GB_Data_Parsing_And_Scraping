e93a13b0134e9e49d8f25a84ab5f7bca5417532a
Filkin-S <52564750+Filkin-S@users.noreply.github.com>
2019-10-22 14:56:41 +0300

    Add files via upload

--- Урок 3. Парсинг HTML. BS, SQLAlchemy/hh-superjob.py
+++ Урок 3. Парсинг HTML. BS, SQLAlchemy/hh-superjob.py
@@ -3,6 +3,7 @@ import requests
 import re
 import pandas as pd
 from pymongo import MongoClient
+from pprint import pprint
 
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'}
 main_link_hh ='https://hh.ru/search/vacancy?text='
@@ -78,15 +79,34 @@ for i in range(pages+1):
                     salary_max = int(salary[0])
         df.loc[len(df)] = [vacancy_title, vacancy_company, vacancy_address, vacancy_link, salary_min, salary_max, salary_cur, 'SuperJob']
 
-# print(df.info())
-# print(df.head().to_string())
-# print(df.tail().to_string())
+print(df.info())
+print(df.head().to_string())
+print(df.tail().to_string())
 
+
+# Подключаемся к MongoDB
 client = MongoClient('localhost',27017)
-db = client['BG256']
+db = client['BG_256']
 vacs = db.vacancies_hh_sj
 
-# vacs.insert_many(df.to_dict('records'))
-df_dict = df.to_dict('records')
 
-# vacs.update_many({}, df_dict, True)
+# Функция для отправки датафрейма в базу
+def insert_mongo(df):
+    vacs.insert_many(df.to_dict('records'))
+
+# Функция для вывода на экран вакансий с зп больше указанной суммы
+def show_min_salary(sal):
+    v_s = vacs.find({'$or': [{'min_salary':{'$gt': sal}}, {'max_salary':{'$gt': sal}}]})
+    for i in v_s:
+        pprint(i)
+
+insert_mongo(df)
+
+show_min_salary(100000)
+
+
+# Функцию update не смог реализовать. По хорошему должна быть одна функция - либо вставляет новые 
+# значения, либо обновляет существующие. Но update не работает по аналогии с insert, с параметрами
+# не разобрался, все время ошибки.
+# vacs.update_many({}, {df_dict}, True)
+
